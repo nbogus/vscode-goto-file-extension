ded7ddfd11d747e84f2a76932d86222acf41a143
nbogusiewicz <nbogusiewicz@infusion.com>
2016-11-29 16:47:11 +0100

    extension impelementation

--- goto-file-extensions/src/extension.ts
+++ goto-file-extensions/src/extension.ts
@@ -10,7 +10,57 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "goto-file-extensions" is now active!');
+     let disposableLess = vscode.commands.registerCommand('dev.component.less', () => {
+        // The code you place here will be executed every time your command is executed
+     let fileName = getFileName(vscode.window.activeTextEditor.document.fileName, 'less');
 
+      vscode.workspace.openTextDocument(fileName)
+            .then((textDocument)=>{
+                vscode.window.showTextDocument(textDocument);               
+            },(e)=>{
+                console.log(e)
+            });
+    });
+
+    let disposableHtml = vscode.commands.registerCommand('dev.component.html', () => {
+        // The code you place here will be executed every time your command is executed
+    let fileName = getFileName(vscode.window.activeTextEditor.document.fileName, 'html');
+
+     vscode.workspace.openTextDocument(fileName)
+            .then((textDocument)=>{
+                vscode.window.showTextDocument(textDocument);               
+            },(e)=>{
+                console.log(e)
+            });
+    });
+
+    let disposableTs = vscode.commands.registerCommand('dev.component.ts', () => {
+        // The code you place here will be executed every time your command is executed
+    let fileName = getFileName(vscode.window.activeTextEditor.document.fileName, 'ts');
+
+     vscode.workspace.openTextDocument(fileName)
+            .then((textDocument)=>{
+                vscode.window.showTextDocument(textDocument);               
+            },(e)=>{
+                console.log(e)
+            });
+    });
+
+    context.subscriptions.push(disposableLess);
+    context.subscriptions.push(disposableHtml);
+    context.subscriptions.push(disposableTs);
+
+}
+
+function getFileName(path:string, extension:string) : string
+{
+    let oldfileName = path.substr(path.lastIndexOf('\\') + 1);
+    let newfileName = oldfileName.substr(0, oldfileName.lastIndexOf(".")) + "." + extension;
+
+    let workspacePath =  path.slice(0,path.lastIndexOf('\\') + 1);
+    let newfilePath = workspacePath + newfileName;
+
+    return newfilePath;
 }
 
 
